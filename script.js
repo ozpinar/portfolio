3a91f56dd012f44704d7c90d32db19697c6753b4
berk <49630457+ozpinar@users.noreply.github.com>
2021-03-15 19:06:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const slider = document.querySelector(".works__slider");
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
+const indicatorParent = document.querySelector(".works__controller ul");
+const menuBtn = document.querySelector(".menu-btn");
+
+
+let sectionIndex = 0;
+
+document.querySelectorAll(".works__controller li").forEach((indicator, idx)=>{
+    indicator.addEventListener('click', () => {
+        sectionIndex = idx;
+        document.querySelector('.works__controller .selected').classList.remove('selected');
+        slider.style.transform = `translate(${(idx) * (-33.3333)}%)`;
+        indicator.classList.add('selected')
+    })
+})
+
+rightArrow.addEventListener('click', () => {
+    sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 0;
+    document.querySelector('.works__controller .selected').classList.remove('selected');
+    indicatorParent.children[sectionIndex].classList.add('selected');
+    slider.style.transform = `translate(${(sectionIndex) * (-33.3333)}%)`;
+})
+
+leftArrow.addEventListener('click', () => {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 2;
+    document.querySelector('.works__controller .selected').classList.remove('selected');
+    indicatorParent.children[sectionIndex].classList.add('selected');
+    slider.style.transform = `translate(${(sectionIndex) * (-33.3333)}%)`;
+})
+
+menuBtn.addEventListener('click', ()=>{
+    menuBtn.classList.toggle('open');
+})
